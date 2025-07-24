585420d0d01718a645ec2a35302e09bab3448ef4
3koo2 <egravedp@gmail.com>
2025-07-24 13:37:45 -0400

    Wrote bouncer2.js which works well. Preview at bouncy_2.html

---
+++ js/bouncer2.js
@@ -0,0 +1,64 @@
+// Bouncer2 by 3koo2
+
+var bouncers = [];
+var lastDelta = 0;
+
+class Bouncer{
+    constructor(id, speed){
+        console.log(id);
+        this.element = document.getElementById(id);
+        console.log(this.element);
+        console.log(innerWidth);
+        console.log(this.element.width);
+
+        this.x=(innerWidth-this.element.width)/2;
+        this.y=(innerHeight-this.element.height)/2;
+        console.log(this.x);
+
+        var dir = Math.random()*Math.PI*2;
+        this.xv=speed*Math.cos(dir); // pixels per second
+        this.yv=speed*Math.sin(dir);
+
+        this.element.style.setProperty("position", "fixed");
+        this.element.style.setProperty("left", 0);
+        this.element.style.setProperty("top", 0);
+    }
+
+    init(){
+        bouncers.push(this);
+    }
+
+    move(delta){
+        if (this.x < 0 || this.x > innerWidth-this.element.width){
+            this.xv*=-1;
+        }
+        if (this.y < 0 || this.y > innerHeight-this.element.height){
+            this.yv*=-1;
+        }
+
+        this.x+=this.xv*delta/1000;
+        this.y+=this.yv*delta/1000;
+
+        this.goTo();
+    }
+
+    goTo(){
+        this.element.style.setProperty("left",this.x+"px");
+        this.element.style.setProperty("top",this.y+"px");
+    }
+}
+
+function loop(d){
+    for (var i = 0; i < bouncers.length; i++){
+        bouncers[i].move(d-lastDelta)
+    }
+    lastDelta=d;
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+// sample useage: 
+
+var feely = new Bouncer("feely", 400);
+feely.init();
